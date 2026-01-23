c7df8776257ee33b41991b8e2fbfeb978a51bab2
MLigowski <mligowski698@technikumkreatywne.pl>
2026-01-23 10:21:29 +0100

    Update_23.01.2026
    everything is working good except resume button

--- Assets/Mateusz_Ligowski_3A/Scripts/pause_scripts/pause_manager_script.cs
+++ Assets/Mateusz_Ligowski_3A/Scripts/pause_scripts/pause_manager_script.cs
@@ -1,33 +1,43 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
     public int pauseSceneIndex = 2;
+    private bool isPaused = false;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseGame();
+            if (!isPaused)
+                PauseGame();
+            else
+                ResumeGame();
         }
     }
 
     public void PauseGame()
     {
+        if (isPaused) return;
+
+        isPaused = true;
         Time.timeScale = 0f;
         SceneManager.LoadScene(pauseSceneIndex, LoadSceneMode.Additive);
     }
 
     public void ResumeGame()
     {
+        if (!isPaused) return;
+
+        isPaused = false;
         Time.timeScale = 1f;
         SceneManager.UnloadSceneAsync(pauseSceneIndex);
     }
 
     public void BackToMainMenu()
     {
-        Time.timeScale = 1f;                 // ⏱️ odmrażamy grę
-        SceneManager.LoadScene(0);           // 🔁 scena 0 = main menu
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 }
